0f35f210d64b062e3554ddbc261b17daa2eb764e
boku <19784872+boku7@users.noreply.github.com>
2021-04-22 17:24:05 -0700

    Update gsSmtp-Csrf2Xss2RCE.py

--- gsSmtp-Csrf2Xss2RCE.py
+++ gsSmtp-Csrf2Xss2RCE.py
@@ -5,7 +5,7 @@
 # Software Link: http://get-simple.info/download/
 # Version: Exploit <= v1.1.1 | Stored XSS <= v1.1.2
 # Tested against Server Host: Windows 10 Pro + XAMPP
-# Tested against Client Browsers: Firefox
+# Tested against Client Browsers: Firefox (Linix & Windows), Internet Explorer
 # Vendor: NetExplorer
 # Exploit Description:
 #   The My SMTP Contact v1.1.2 plugin for GetSimple CMS suffers from a Stored Cross-Site Scripting (XSS) vulnerability, that when chained together with the CSRF vulnerability in v1.1.1, allows remote unauthenticated attackers to achieve Remote Code Execution on the hosting server, when an authenticated administrator visits a malicious third party website. The PHP function htmlspecialchars() attempts to sanitize the user-input, but is trivially bypassed by passing the dangerous characters as escaped hex bytes. This allows attackers to breakout of the HTML rendered by the PHP engine, to run arbitrary client-side code within the admins browser; after the admin submits the POST request from the CSRF attack. Since GetSimple CMS suffers from a known PHP code injection vulnerability within the themes edit page, the attacker can ride the admins session to perform a chain of XHR requests within the admins browser. The XHR chain triggered by the CSRF attack will collect the CSRF Token from the themes edit page, and use the token to exploit the PHP Code Injection vulnerability to upload a webshell within every page hosted by the CMS.
